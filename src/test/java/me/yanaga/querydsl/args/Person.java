a1c518912d9d6dd32dd0e504e601278a1157b763
Edson Yanaga <edson@yanaga.com.br>
2015-02-24 11:43:17 -0300

    Added travis-ci configuration. Added maven-license-plugin configuration.

--- src/test/java/me/yanaga/querydsl/args/Person.java
+++ src/test/java/me/yanaga/querydsl/args/Person.java
@@ -1,9 +1,34 @@
 package me.yanaga.querydsl.args;
 
+/*
+ * #%L
+ * queydsl-args
+ * %%
+ * Copyright (C) 2014 - 2015 Edson Yanaga
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import javax.persistence.Entity;
+import java.math.BigDecimal;
 
 @Entity
 public class Person {
+
+	private String name;
+
+	private String surname;
 
 	private Integer someInteger;
 
@@ -12,6 +37,26 @@ public class Person {
 	private Long someLong;
 
 	private Long anotherLong;
+
+	private BigDecimal amount;
+
+	private BigDecimal anotherAmount;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
 
 	public Integer getSomeInteger() {
 		return someInteger;
@@ -44,4 +89,21 @@ public class Person {
 	public void setAnotherLong(Long anotherLong) {
 		this.anotherLong = anotherLong;
 	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+
+	public BigDecimal getAnotherAmount() {
+		return anotherAmount;
+	}
+
+	public void setAnotherAmount(BigDecimal anotherAmount) {
+		this.anotherAmount = anotherAmount;
+	}
+
 }
